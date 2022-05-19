2717453895559092d220db764f4e89054c5cfb23
Volodymyr Kit <v.kit@maytech.net>
2022-05-19 22:44:26 +0300

    remove default path

--- vars.go
+++ vars.go
@@ -5,8 +5,7 @@ import "time"
 const version = 2 // ipc package version
 
 const (
-	minMsgSize            = 1024
-	defaultMaxMsgSize     = 3145728 // 3Mb  - Maximum bytes allowed for each message
-	defaultSocketBasePath = "/tmp"
-	defaultRetryTimer     = 200 * time.Millisecond
+	minMsgSize        = 1024
+	defaultMaxMsgSize = 3145728 // 3Mb  - Maximum bytes allowed for each message
+	defaultRetryTimer = 200 * time.Millisecond
 )
